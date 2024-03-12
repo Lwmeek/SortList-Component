704d089920ce689df5d1481f910e692c4a3347ce
Timic Ivey <timicivey27@gmail.com>
2024-03-12 14:26:02 -0400

    Starting phase of project getting outline

---
+++ src/Components/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Heading = () => {
+	return <h1>Select your top 5 tech skills</h1>;
+};
+
+export default Heading;
---
+++ src/Components/SkillsList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SkillsList = () => {
+	return (
+		<>
+			<div className="skillList-container w-96 h-96 bg-black rounded-lg">
+				<section className="text-white flex justify-between">
+					<button>JavaScript</button>
+                    <h2>Suggested Skills</h2>
+				</section>
+			</div>
+		</>
+	);
+};
+
+export default SkillsList;
