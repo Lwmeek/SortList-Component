d4f39511889776d88f7ba3670e586d025988508d
Timic Ivey <timicivey27@gmail.com>
2024-03-12 15:57:34 -0400

    Addint icons needed couple of color changes

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Heading = () => {
-	return <h1 className="font-semibold text-4xl mb-8">Select your top 5 tech skills</h1>;
+	return <h1 className="font-semibold text-4xl mb-8 text-navyblue">Select your top 5 tech skills</h1>;
 };
 
 export default Heading;
--- src/Components/SkillsList.jsx
+++ src/Components/SkillsList.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { ChevronDownIcon, PlusIcon } from "@heroicons/react/24/solid";
 
 const SkillsList = () => {
 	return (
 		<>
-			<div className="skillList-container w-96 h-96 bg-black rounded-lg">
-				<section className="text-white flex justify-between">
-					<button>JavaScript</button>
-                    <h2>Suggested Skills</h2>
+			<div className="skillList-container bg-white rounded-2xl p-16">
+				<section className="text-white flex gap-x-16 justify-center">
+					<button className="bg-navyblue text-lg font-medium rounded-lg p-4">
+						JavaScript
+					</button>
+					<div>
+						<h2 className="text-navyblue font-semibold text-base">
+							Suggested Skills
+						</h2>
+						<p className="text-paleblue font-normal text-base">HTML</p>
+						<ChevronDownIcon className="text-navyblue w-5" />
+                        <PlusIcon className="text-navyblue w-4"/>
+					</div>
 				</section>
 			</div>
 		</>
